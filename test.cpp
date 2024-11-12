b59bee7827c40d961958b218500785185aeb5794
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2024-11-12 11:02:38 +0530

    code

---
+++ test.cpp
@@ -0,0 +1,22 @@
+arr1=[1 2 3 4 5 6 ]
+int a=5;
+int *pt=&a;
+//100+4
+int solve(int pt){
+*pt++;
+return *pt;
+}
+
+arr2=[1 2 3 4 5 6]
+multiset1
+target =n
+x+ y=n
+
+nlogn
+n*logn
+1  3 4 5
+int [10^6]
+Soumen Ghosh Tim Roughgarden
+
+
+
